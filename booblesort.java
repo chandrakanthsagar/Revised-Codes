0f861f29c9c0911321c315a57378c0e83a56116a
chandrakanthsagar <109515216+chandrakanthsagar@users.noreply.github.com>
2023-08-24 10:58:50 +0530

    Update booblesort.java

--- booblesort.java
+++ booblesort.java
@@ -8,8 +8,8 @@ public class booblesort{
             for(int j=0;j<n-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
+                    arr[j]=arr[j+1];
+                     arr[j+1]=temp;
                     c++;
                     flag=true;
                 }
